dad9da0451cf674179bb54d6c55e7cc7f92792d7
Maria Zak <mzakart@gmail.com>
2023-08-18 16:31:41 -0400

    passport.js work in progress

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const helmet = require('helmet');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
 const knex = require('./db/db');
+const expressSession = require('express-session');
+const helmet = require('helmet');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const storiesRoutes = require('./routes/storiesRoutes.js');
 const genresRoutes = require ('./routes/genresRoutes.js');
