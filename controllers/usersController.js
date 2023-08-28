ea820551454454f7d01a4b73c7fad65ce860ab52
Maria Zak <mzakart@gmail.com>
2023-08-27 23:18:24 -0400

    Updated updateUser to receive links, username

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -31,22 +31,69 @@ exports.getUserById = (req, res) => {
 
 exports.updateUser = (req, res) => {
   const { id } = req.params;
-  const { username, email } = req.body;
-  
-  knex('users')
-    .where('id', id)
-    .update({
-      username,
-      email,
-    })
-    .then(() => {
-      res.json({ message: 'User updated successfully.' });
-    })
-    .catch((err) => {
-      console.error('Error updating user:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    });
+  const { bio, pen_first_name, pen_last_name, links, username } = req.body;
+
+  const updateData = {};
+
+  if (bio !== undefined) {
+    updateData.bio = bio;
+  }
+
+  if (pen_first_name !== undefined) {
+    updateData.pen_first_name = pen_first_name;
+  }
+
+  if (pen_last_name !== undefined || pen_last_name === null) {
+    // Update pen_last_name only if it's provided or explicitly set to null
+    updateData.pen_last_name = pen_last_name;
+  }
+
+  if (username !== undefined) {
+    updateData.username = username;
+  }
+
+  // Assuming links is an array of strings
+  if (Array.isArray(links)) {
+    // Handle updating the links if provided
+    // First, delete existing links for the user
+    knex('users')
+      .where('id', id)
+      .update({ links: null }) // Clear existing links
+      .then(() => {
+        // Then, insert new links
+        return knex('users')
+          .where('id', id)
+          .update({ links: JSON.stringify(links) });
+      })
+      .then(() => {
+        // Update other fields
+        return knex('users')
+          .where('id', id)
+          .update(updateData);
+      })
+      .then(() => {
+        res.json({ message: 'User updated successfully.' });
+      })
+      .catch((err) => {
+        console.error('Error updating user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  } else {
+    // Update user's bio, pen names, and username (if provided), but not links
+    knex('users')
+      .where('id', id)
+      .update(updateData)
+      .then(() => {
+        res.json({ message: 'User updated successfully.' });
+      })
+      .catch((err) => {
+        console.error('Error updating user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  }
 };
+
+
 
 exports.deleteUser = (req, res) => {
   const { id } = req.params;
