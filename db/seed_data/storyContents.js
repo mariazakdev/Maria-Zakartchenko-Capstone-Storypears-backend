ef7422d18fd4b8d9c4298dccbca2deb6ded5524c
Maria Zak <mzakart@gmail.com>
2023-08-28 14:46:24 -0400

    Updated story_contents table

--- db/seed_data/storyContents.js
+++ db/seed_data/storyContents.js
@@ -1,12 +1,12 @@
 const seedData = [
   {
-    id: 1, 
+    id: 'aee0ae65-b36d-418f-af5e-b0bddb8e9d5f', 
     user_id: 1, 
     story_id: 'story1',
     content: 'This is the first contribution.',
   },
   {
-    id: 2, 
+    id: '2a3de7d8-408c-4f31-88d5-ea4d7fdc3943', 
     user_id: 2, 
     story_id: 'story1',
     content: 'This is the second contribution.',
