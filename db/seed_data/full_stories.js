198d6ebef782fc5af6cba62893d7e81e07b211fd
Maria Zak <mzakart@gmail.com>
2023-09-02 19:37:47 -0400

    Added to full_stories seed

--- db/seed_data/full_stories.js
+++ db/seed_data/full_stories.js
@@ -162,7 +162,7 @@ module.exports = [
           {
             id: 56,
             story_id: "f5cc11b6-49e1-11ee-be56-0242ac120002",
-            user_id: 65,
+            user_id: 67,
             emotion: null,
             genre: "Comedy",
             created_at: "2023-09-01T16:23:49.000Z",
@@ -190,7 +190,7 @@ module.exports = [
           {
             id: 57,
             story_id: "f5cc11b6-49e1-11ee-be56-0242ac120002",
-            user_id: 64,
+            user_id: 68,
             emotion: null,
             genre: "Comedy",
             created_at: "2023-09-01T16:24:49.000Z",
@@ -226,6 +226,164 @@ module.exports = [
               Amidst the laughter and amusement, Jerry realized something. His 'problematic self' wasn’t him—it was his comedic house! And as bizarre as it was, it sure made life entertaining.
               
               And so, in a world filled with challenges and stress, Jerry's house brought joy, laughter, and unexpected comedy to all who entered. After all, who needs a therapist when you have household appliances with impeccable comedic timing?`,
+          },
+        ],
+    },
+    {
+        id: "42ad4d28-49e8-11ee-be56-0242ac120002",
+        stories_data: [
+          {
+            id: 56,
+            story_id: "4c8217ac-49e8-11ee-be56-0242ac120002",
+            user_id: 67,
+            emotion: null,
+            genre: "Comedy",
+            created_at: "2023-09-01T16:23:49.000Z",
+            updated_at: "2023-09-01T16:23:49.000Z",
+            title: "Galary Inside Out",
+            content:
+              `In the vast realm of Cyndara, galaxies spun and danced, and one such galaxy was the “Galary.” Unlike others, this galaxy had a unique feature: it was turned inside out. What was once core became periphery, and the vast dark expanse now lay nestled deep at its center. At the heart of Galary was the mystic realm of Introspecta.
+
+              Introspecta, home to the Illuminates, was a realm where thoughts materialized and memories crystallized. Here, the external was driven by the internal. Emotions influenced weather, desires built structures, and dreams painted the landscape.
+              
+              Lyria, a young Illuminate, was born with an unprecedented ability: she could extract memories. Not just view, but physically pluck them out, turning them into shimmering orbs of light. While this gift was prized, it also became her curse. People, fearing the exposure of their darkest secrets, shunned her.
+              
+              One evening, as Lyria wandered the Musing Meadows – a place where daydreams floated as luminescent butterflies – she stumbled upon an ancient, hidden chamber. It was the Archive of Epochs, where the memories of the entire galaxy were stored.`,
+          },
+          {
+            id: 57,
+            story_id: "4c8217ac-49e8-11ee-be56-0242ac120002",
+            user_id: 68,
+            emotion: null,
+            genre: "Comedy",
+            created_at: "2023-09-01T16:24:49.000Z",
+            updated_at: "2023-09-01T16:24:49.000Z",
+            title: "Galary Inside Out",
+            content:
+              `Intrigued, Lyria entered and discovered a shocking truth. Galary was not naturally inside out; it had been turned that way. A dark power, The Void Whisperer, desired the core's energy, the emotions and memories of Introspecta, to fuel his quest for cosmic dominance. He inverted the galaxy, making Introspecta vulnerable.
+
+              Determined to set things right, Lyria embarked on a journey. Using her powers, she dove into memories, seeking knowledge, allies, and weapons. She journeyed to the fringes of Galary, meeting creatures like Stellarnox, beings of starlight, and Nebul, the cloud-shaper.
+              
+              However, the Void Whisperer, sensing her intentions, unleashed the Shadows of Doubt and the Fearlings to stop her.
+              
+              As battles raged, Lyria's strength was her profound understanding of emotions. She used joy to blind Fearlings and mustered courage to banish Doubt. With Nebul, she conjured storms of hope, and Stellarnox wielded weapons forged from determination.
+              
+              But the final confrontation was inevitable. In the very heart of Introspecta, Lyria faced the Void Whisperer. Their clash was a spectacle, not of physical might, but of emotional depth. Despair clashed with hope, fear with courage, and hate with love.`,
+          },
+          {
+            id: 58,
+            story_id: "4c8217ac-49e8-11ee-be56-0242ac120002",
+            user_id: 63,
+            emotion: null,
+            genre: "Comedy",
+            created_at: "2023-09-01T16:25:49.000Z",
+            updated_at: "2023-09-01T16:25:49.000Z",
+            title: "Galary Inside Out",
+            content:
+              `The battle seemed even, but Lyria had an edge. Drawing from the Archive of Epochs, she summoned the collective hope and dreams of Galary. The sheer force of positive emotion overwhelmed the Void Whisperer.
+
+              Defeated, the dark power was expelled, and slowly, with the combined might of all beings in Galary, the galaxy started reverting. It was no longer inside out.
+              
+              Introspecta, once vulnerable, was now safely ensconced at the galaxy's core, protected by layers of stars, planets, and cosmic wonders.
+              
+              Lyria, once an outcast, became a beacon of hope, uniting the varied beings of Galary. And as tales of her bravery spread, so did a profound understanding: It's the inside that shapes the outside.
+              
+              In the restored Galary, memories, feelings, and dreams were celebrated, shared, and cherished, reinforcing the age-old wisdom that true power lies within.`,
+          },
+          {
+            id: 58,
+            story_id: "4c8217ac-49e8-11ee-be56-0242ac120002",
+            user_id: 63,
+            emotion: null,
+            genre: "Fantasy",
+            created_at: "2023-09-01T16:25:49.000Z",
+            updated_at: "2023-09-01T16:25:49.000Z",
+            title: "Galary Inside Out",
+            content:
+              `As the newly minted savior of Galary, Lyria's journey didn’t end with the defeat of the Void Whisperer. The scars left by his reign lingered. Whole worlds within Galary had forgotten joy, while others were trapped in endless loops of their worst memories.
+
+              Lyria, along with Stellarnox and Nebul, formed the Luminary League, dedicated to healing the broken reaches of Galary. The trio ventured to the Torn Continents of Reminiscence, islands floating in the cosmic sea, each stuck in a tragic past.
+              
+              Their first stop was Chronotide Isle, where time itself seemed to be in shreds. Here, moments of past, present, and future occurred simultaneously, leaving its inhabitants in constant confusion. With her unique ability, Lyria gently separated each time strand, weaving them into a coherent thread. As she did, the island's denizens began to understand their past, live in the present, and dream of the future.
+              
+              Then, on the Mirror Moors, the League encountered a society trapped in vanity and narcissism, obsessed with their reflections which showed not their faces, but their desires. These mirror-visions became so consuming that the real world faded for them. Lyria, using a memory from the Archive of Epochs, showed them the story of Narcissus, helping them break free from their self-imposed illusions.`,
+          },
+          {
+            id: 58,
+            story_id: "4c8217ac-49e8-11ee-be56-0242ac120002",
+            user_id: 63,
+            emotion: null,
+            genre: "Fantasy",
+            created_at: "2023-09-01T16:25:49.000Z",
+            updated_at: "2023-09-01T16:25:49.000Z",
+            title: "Galary Inside Out",
+            content:
+              `As the newly minted savior of Galary, Lyria's journey didn’t end with the defeat of the Void Whisperer. The scars left by his reign lingered. Whole worlds within Galary had forgotten joy, while others were trapped in endless loops of their worst memories.
+
+              Lyria, along with Stellarnox and Nebul, formed the Luminary League, dedicated to healing the broken reaches of Galary. The trio ventured to the Torn Continents of Reminiscence, islands floating in the cosmic sea, each stuck in a tragic past.
+              
+              Their first stop was Chronotide Isle, where time itself seemed to be in shreds. Here, moments of past, present, and future occurred simultaneously, leaving its inhabitants in constant confusion. With her unique ability, Lyria gently separated each time strand, weaving them into a coherent thread. As she did, the island's denizens began to understand their past, live in the present, and dream of the future.
+              
+              Then, on the Mirror Moors, the League encountered a society trapped in vanity and narcissism, obsessed with their reflections which showed not their faces, but their desires. These mirror-visions became so consuming that the real world faded for them. Lyria, using a memory from the Archive of Epochs, showed them the story of Narcissus, helping them break free from their self-imposed illusions.
+              
+              But not all lands welcomed their aid. On the Obsidian Atoll, the inhabitants, known as the Shadebound, had become addicted to their sorrows. They believed their painful memories gave them depth and purpose. Stellarnox, relating to their pain as beings of starlight often burned out too soon, shared tales of stars reborn, illustrating the beauty of moving forward. With time, the Shadebound started releasing their grip on their haunting past.
+              
+              Throughout their voyages, the Luminary League faced challenges from within as well. Stellarnox struggled with the transient nature of her starlight existence, and Nebul grappled with his cloudy, ever-changing identity. Lyria, though a beacon of hope, had her own shadows to confront - her isolation as a child and the weight of her unique gift.`,
+          },
+        ],
+    },
+    {
+        id: "c7252404-49e8-11ee-be56-0242ac120002",
+        stories_data: [
+          {
+            id: 56,
+            story_id: "d02d3730-49e8-11ee-be56-0242ac120002",
+            user_id: 67,
+            emotion: null,
+            genre: "Horror",
+            created_at: "2023-09-01T16:23:49.000Z",
+            updated_at: "2023-09-01T16:23:49.000Z",
+            title: "Jello Face",
+            content:
+              `Amelia, a culinary enthusiast, often experimented with her dishes. She was the talk of the town, especially for her themed dinners. One autumn evening, she decided to create a dish inspired by Halloween - edible jello masks. The concept was simple: mould a person’s face with jello, and they'd wear it momentarily before eating it.
+
+              She invited a group of close friends for the dinner party. The table gleamed with candelabras, fine china, and silverware, while eerie orchestral music played in the background. As her friends arrived, they marveled at her setup. The highlight, of course, was the jello face-making station.
+              
+              One by one, guests lay down as a mould of their face was taken. The jello was then poured in, set quickly using a special ingredient, and then the mask was worn during dinner. It was creepy yet fascinating, as everyone sat eating with a jello version of their face obscuring their real one.
+              
+              As dinner progressed, guests started to feel a slight itch beneath their jello masks. Thinking it was just the sensation of the jello, they ignored it. However, the itch soon intensified to a burning sensation. Frantic, they tried to remove their jello masks, but the masks wouldn't budge. Panic set in.`,
+          },
+          {
+            id: 57,
+            story_id: "d02d3730-49e8-11ee-be56-0242ac120002",
+            user_id: 66,
+            emotion: null,
+            genre: "Horror",
+            created_at: "2023-09-01T16:24:49.000Z",
+            updated_at: "2023-09-01T16:24:49.000Z",
+            title: "His Red Hands",
+            content:
+              `Amelia, horrified, tried to assist, but the jello seemed to have adhered to their skin. One guest, in desperation, used a knife to try and cut the mask off, only to find that it wasn’t just attached to the skin, but seemed to have fused with it.
+
+              As the night progressed, guests' personalities began to warp. The masks, now inseparable from their faces, started influencing their behaviors. The quietest of the lot became aggressively loud, the always cheerful one turned morose, and the kindest person became the meanest.
+              
+              Desperate for a solution, Amelia scoured her recipe, realizing the catalyst was an ancient powder she'd bought from a mysterious old man during her travels, thinking it was just a quick setting agent for jello. The label, in faded ink, read: “Spiritus Faciei” - Spirit of the Face.
+              
+              Realizing she had unleashed something dark and ancient, Amelia needed to find a way to reverse the effects. Recalling the old man who sold her the powder, she remembered his cryptic words: "The spirit needs acceptance to truly set."
+              
+              The key was acceptance. Amelia quickly relayed her epiphany to her friends. They had to embrace the feelings and personalities the masks were imposing on them. It was an excruciating process, confronting amplified, twisted versions of themselves. But as each guest came to terms with their altered selves, the jello masks began to loosen, eventually slipping off, leaving behind their original faces, untouched but for tear-streaked trails.`,
+          },
+          {
+            id: 58,
+            story_id: "d02d3730-49e8-11ee-be56-0242ac120002",
+            user_id: 65,
+            emotion: null,
+            genre: "Horror",
+            created_at: "2023-09-01T16:25:49.000Z",
+            updated_at: "2023-09-01T16:25:49.000Z",
+            title: "His Red Hands",
+            content:
+              `As dawn broke, the traumatized group gathered their wits and left Amelia's house, forever scarred by the horrors of the jello face. And Amelia, her love for culinary experiments deeply shaken, decided to stick to simpler recipes, far away from any mysterious powders or ancient spirits.`,
           },
         ],
     },
