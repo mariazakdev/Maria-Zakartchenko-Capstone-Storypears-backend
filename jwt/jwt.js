862ff4ceaa3b554fb962c5f7d026de4396fe3859
Maria Zak <mzakart@gmail.com>
2023-09-01 21:09:02 -0400

    profile has data

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,37 +1,51 @@
 const { sign, verify } = require("jsonwebtoken");
 const { jwtSecret } = require('../config'); 
 
+/**
+ * Creates a JWT for a given user.
+ * @param {Object} user - The user object.
+ * @returns {string} - The generated JWT.
+ */
 const createTokens = (user) => {
-  const accessToken = sign(
-    { id: user.id },
-    jwtSecret
-  );
-    // no expiration time, return with refresh token
+  console.log("User Data for Token:", user);
+  const accessToken = sign({ id: user.id }, jwtSecret, { expiresIn: '48h' });
+  console.log("Generated Access Token:", accessToken);
   return accessToken;
 };
 
-
-
-
-
-
+/**
+ * Middleware to validate the JWT in the request.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {Function} next - The Express next middleware function.
+ */
 const validateToken = (req, res, next) => {
-  const accessToken = req.cookies["access-token"];
+  console.log("Inside validateToken Middleware.");
+  console.log("Received Cookies:", req.cookies);
+  
+  const accessToken = req.cookies["pearCookie"];
 
-  if (!accessToken)
-    return res.status(400).json({ error: "User not Authenticated!" });
+  if (!accessToken) {
+    return res.status(401).json({ error: "Access token is missing. User not authenticated." });
+  }
 
   try {
     const decodedToken = verify(accessToken, jwtSecret);
+    console.log("Decoded Token Data:", decodedToken);
 
-    // Set the decoded user data to req.user
+    // Attach the decoded user data to the request object.
     req.user = decodedToken;
 
     return next();
   } catch (err) {
-    return res.status(400).json({ error: err });
+    console.error("Error while verifying token:", err);
+    // JWT verification can fail for multiple reasons, such as token expiration or token tampering.
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: "Access token has expired. Please log in again." });
+    } else {
+      return res.status(403).json({ error: "Invalid access token." });
+    }
   }
 };
-
 
 module.exports = { createTokens, validateToken };
